684e0c034b89c93e1b0ee6b3b7ba352e1e77941a
InoueSota <asitanobento87@gmail.com>
2019-03-28 21:58:56 +0900

    動作確認用サンプルメソッド追加

---
+++ Assets/Playground/Scripts/TargetMethodsForTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Playground
+{
+    public class TargetMethodsForTest : MonoBehaviour
+    {
+
+        public void Method01()
+        {
+            Debug.Log("Method01 is called!");
+        }
+        public void Method02()
+        {
+            Debug.Log("Method02 is called!");
+        }
+        public void Method03()
+        {
+            Debug.Log("Method03 is called!");
+        }
+    }
+}
