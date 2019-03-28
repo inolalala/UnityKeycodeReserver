6344cece3dc6e112303ebef9fbfcd149bdf7dfdc
InoueSota <asitanobento87@gmail.com>
2019-03-28 21:51:26 +0900

    考えてることの基本的な部分は作ったので、機能追加・リファクタリングして使いやすいAPIに整備する

---
+++ Assets/Scripts/KeyCodeReserver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyCodeReserver : MonoBehaviour {
+
+    [SerializeField] private List<KeyEvent> keys = new List<KeyEvent>();
+
+    [Serializable]
+    public class KeyEvent
+    {
+        public KeyCode key;
+        public UnityEvent keyEvent;
+    }
+
+    public List<KeyEvent> GetUsingKeyCodes()
+    {
+        return keys;
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < GetUsingKeyCodes().Count; i++)
+        {
+            KeyEvent pressedKeyCode = GetUsingKeyCodes()[i];
+            if (Input.GetKeyDown(pressedKeyCode.key))
+            {
+                pressedKeyCode.keyEvent.Invoke();
+            }
+        }
+    }
+}
